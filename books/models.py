5539495aa6e33b390ba686fa9942865625d55a1d
Pavankumar <pavanvp1299@gmail.com>
2026-03-02 23:19:32 +0530

    changed Python version

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    discription = models.TextField(default="No description available")
+    discription = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
