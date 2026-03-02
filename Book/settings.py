95941e2070896ee59076145f40411c6a64cb7cca
Pavankumar <pavanvp1299@gmail.com>
2026-03-02 21:47:49 +0530

    Switch DB to SQLite for Render deployment

--- Book/settings.py
+++ Book/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'Book.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'book_db',
-        'USER': 'root',
-        'PASSWORD': '1299',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
